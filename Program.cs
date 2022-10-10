6b1a19393d9969a70fa503102cc00af0e2b9a610
Everaldo Monteiro <everaldo.monteiro.jr@gmail.com>
2022-10-10 20:41:42 -0300

    Desafio Celular POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+System.Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("123456", "Modelo 1", "111111111", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+System.Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("4987", "Modelo 2", "222222222", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
